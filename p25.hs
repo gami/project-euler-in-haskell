4b67a0c52eacfed14f91acd459835fdd2f82fdae
Masaki Takegami <takegamm@gmail.com>
2010-03-12 23:42:32 +0900

    p25 done

--- p25.hs
+++ p25.hs
@@ -1,4 +1,5 @@
-main = print $ show $ length $ takeWhile (\x -> (digitCount (show x)) < 1000) (map fastfib [1..])
+--main = print $ show $ length $ takeWhile (\x -> (digitCount (show x)) < 5) (map fastfib [1..])
+main = print $ show $ 1 + (length (takeWhile (\x -> (digitCount (show x)) < 1000) (map fastfib [1..])))
 --main = print $ show $ takeWhile (\x -> (digitCount (show x)) < 3) [1..1000]
 --main = print $ show $ (digitCount (show 1234) == 4) 
 
@@ -6,7 +7,7 @@ fastfib :: Integer -> Integer
 fastfib n = fst (fibPair n)
 
 fibPair :: Integer -> (Integer, Integer)
-fibPair 1 = (1, 2)
+fibPair 1 = (1, 1)
 fibPair n = fibStep(fibPair(n-1))
 
 fibStep :: (Integer, Integer) -> (Integer, Integer)
