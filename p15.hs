ecf87b2d91d40d6f2652674e35051cdb33ac5369
Masaki Takegami <takegamm@gmail.com>
2010-03-12 12:40:14 +0900

    p15: done

--- p15.hs
+++ p15.hs
@@ -1,6 +1,9 @@
-main = print $ show $ combi 39 20
+main = print $ show $ combi 40 20
 
-combi :: Integer -> Integer -> Integer
-combi a b
-    | a > (a-b) = a * (combi (a-1) b)
-    | otherwise = 1
+--combi :: Integer -> Integer -> Integer
+combi a b = truncate(fromIntegral(frac a b)/fromIntegral(frac b b))
+    
+frac a b
+    | a <= 1 = 1
+    | b <  1 = 1
+    | otherwise = a * (frac (a-1) (b-1))
