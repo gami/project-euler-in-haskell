b438be1937e9c7b774c7c153d4442a253492dad9
Masaki Takegami <takegamm@gmail.com>
2010-03-13 23:48:05 +0900

    implement ackerman function

--- p282.hs
+++ p282.hs
@@ -1,6 +1,16 @@
-main = print $ show $ ackerman 5 5
+main = print $ show $ sum $ map (\x -> ackerman2 x x) [1..6]
 
 ackerman m n
     |m==0 = n+1
     |m > 0 && n == 0 = ackerman (m-1) 1
     |m > 0 && n > 0 = ackerman (m-1) (ackerman m (n-1))
+
+ackerman2 m n
+    |m==0 = n+1
+    |m==1 = n+2
+    |m==2 = 2*n+3
+    |m==3 = 2^(n+3)-3
+    |m > 3 && n == 0 = ackerman2 (m-1) 1
+    |m > 3 && n > 0 = ackerman2 (m-1) (ackerman2 m (n-1))
+
+
