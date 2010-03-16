06cfc7c800b9ac75f81adca1e7cac194d2e8c0dd
Masaki Takegami <takegamm@gmail.com>
2010-03-16 12:37:48 +0900

    p14 done

--- p14.hs
+++ p14.hs
@@ -1,15 +1,19 @@
 import List
 
---main = print $ show $ last $ sort $ map length $ mcollatz [1..20000] []
-main = print $ show $ mcollatz [1..20] []
+main = print $ show $ snd $ last $ sort $ mcollatz [1..1000000]
+--main = print $ show $ last $ mcollatz [1..5000] []
 
-mcollatz [] ms = []
-mcollatz (n:ns) ms = do let cl = collatz n ms
-                        cl:(mcollatz ns (ms++[cl]))
-
-collatz :: Integer -> [[Integer]] -> [Integer] 
-collatz n ms
+mcollatz :: [Integer] -> [(Integer,Integer)]
+mcollatz [] = []
+mcollatz (n:ns) = (toInteger (length ( collatz n)),n):(mcollatz ns)
+                        
+--fcollatz [] ms = []
+--fcollatz (n:ns) ms = do let cl = collatz n ms
+--                        cl:(fcollatz (filter (\x -> not (elem x cl)) ns) ms)
+                                                
+collatz :: Integer -> [Integer] 
+collatz n
     |n==1 = [1]
-    |ms /= [] && n < toInteger (length ms) = ms !! (fromInteger (n-1))
-    |even n = n:(collatz (truncate ((fromIntegral n)/(fromIntegral 2)))　ms)
-    |otherwise = n:(collatz (3*n + 1) ms)
+--    |n < ((last ms) !! 0) = ms !! (fromInteger (n-1))
+    |even n = n:(collatz (truncate ((fromIntegral n)/(fromIntegral 2))))
+    |otherwise = n:(collatz (3*n + 1))
