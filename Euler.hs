94b25e498e482fdb4cfa3ec8b1416ad92fddc2f0
Masaki Takegami <takegamm@gmail.com>
2010-03-17 08:31:35 +0900

    refactor about divisor

--- Euler.hs
+++ Euler.hs
@@ -1,4 +1,4 @@
-module Euler(parseData, triangle)
+module Euler(parseData, triangle, primes, primeFactors, divisor)
 
 where
     parseData t = map (\x -> map (\s -> read s::Integer ) (words x)) $ lines t
@@ -13,4 +13,27 @@ where
         | n == 0 = (y+(ts !! (fromIntegral n))):(triangle' ys ts (n+1))
         | toInteger(length(ts)) == n = [y+(ts !! (fromIntegral (n-1)))]
         | otherwise = (y+(max (ts !! (fromIntegral (n-1))) (ts !! (fromIntegral n) ))):(triangle' ys ts (n+1))
+        
+    primes :: Integer -> [Integer]
+    primes m = sieve (2:[3,5..m])
+
+    sieve (p:xs)
+        | xs == [] = [p]
+        | otherwise = p:(sieve [x | x <- xs, x `mod` p /=0])
     
+    primeFactors :: Integer -> [Integer] -> [Integer]
+    primeFactors n (p:ps)
+        | n < 2 = []
+        | (p:ps) == [] = []
+        | (mod n p) == 0 = p:(primeFactors (divide n p) (p:ps))
+        | otherwise = primeFactors n ps
+    divide :: Integer -> Integer -> Integer
+    divide a 0 = error("zero divide")
+    divide a b = truncate( fromIntegral(a) / fromIntegral(b) )
+    
+    divisor :: Integer -> [Integer]
+    divisor n = [1]++(divisor2 n [2..(n-1)])++[n]
+    divisor2 n [] = []
+    divisor2 n (x:xs) 
+        | (mod n x)==0 = x:divisor2 n xs
+        | otherwise = divisor2 n xs
--- p12.hs
+++ p12.hs
@@ -1,3 +1,4 @@
+import Euler
 
 main = print $ show $ tri $ toInteger $ length (takeWhile (< 500) (map (\x -> triDivCount x ps) [1..]))+1
        where ps = primes 2000000
@@ -18,29 +19,3 @@ divc (x:xs)
 
 cnt n xs = filter (== n) xs
 ncnt n xs = filter (/= n) xs
-
---length $ divisor n
-
-divisor n = [1]++(divisor2 n [2..(n-1)])++[n]
-
-divisor2 n [] = []
-divisor2 n (x:xs) 
-    | (mod n x)==0 = x:divisor2 n xs
-    | otherwise = divisor2 n xs
-
-primeFactors :: Integer -> [Integer] -> [Integer]
-primeFactors n (p:ps)
-    | n < 2 = []
-    | (p:ps) == [] = []
-    | (mod n p) == 0 = p:(primeFactors (divide n p) (p:ps))
-    | otherwise = primeFactors n ps
-
-divide :: Integer -> Integer -> Integer
-divide a 0 = error("zero divide")
-divide a b = truncate( fromIntegral(a) / fromIntegral(b) )
-
-primes m = sieve (2:[3,5..m])
-
-sieve (p:xs)
-    | xs == [] = [p]
-    | otherwise = p:(sieve [x | x <- xs, x `mod` p /=0])
